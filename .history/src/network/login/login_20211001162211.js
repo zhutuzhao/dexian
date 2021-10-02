0d42c3f67a0187f6baf68c9a8d0d973b83bb8502
792810749@qq.com <792810749@qq.com>
2021-10-02 12:01:49 +0800

    remove .history file from remote

--- .history/src/network/login/login_20211001162211.js
+++
@@ -1,8 +0,0 @@
-import {requst} from "../requst.js";
-
-export function loginRequest(account, password, role){
-    console.log("456");
-    return requst({
-        url:`/web/login/in?account=${account}&password=${password}&role=${role}`,
-    });
-}
