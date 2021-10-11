d36180f5acc30e673026b916819891f9ffeba554
zhutuzhao <810870471@qq.com>
2021-10-12 01:04:37 +0800

    咨询页

--- src/network/business/business.js
+++ src/network/business/business.js
@@ -1,6 +1,7 @@
 import { requst } from "../requst.js";
 
 export function get_all() {
+  //获取所有的咨询信息
   const formdata = new FormData();
   formdata.append("id", localStorage.getItem("id"));
   return requst({
@@ -11,6 +12,7 @@ export function get_all() {
 }
 
 export function get_no_reply() {
+  //商家获取所有未回复的咨询
   const formdata = new FormData();
   formdata.append("id", localStorage.getItem("id"));
   return requst({
@@ -21,6 +23,7 @@ export function get_no_reply() {
 }
 
 export function show_consult(consult_id) {
+  //是否展示该条咨询
   return requst({
     url: `/api/web/consult/show`,
     method: "GET",
@@ -31,6 +34,7 @@ export function show_consult(consult_id) {
 }
 
 export function delete_consult(consult_id) {
+  //删除某条咨询
   const formdata = new FormData();
   formdata.append("id", localStorage.getItem("id"));
   formdata.append("consultId", consult_id);
@@ -40,29 +44,28 @@ export function delete_consult(consult_id) {
     data: formdata,
   });
 }
-// export function searchByTime(startTime, endTime, pageNum, pageSize, status) {
-//   var formdata = new FormData();
-//   formdata.append("accountId", localStorage.getItem("id"));
-//   formdata.append("startTime", startTime);
-//   formdata.append("endTime", endTime);
-//   formdata.append("pageNum", pageNum);
-//   formdata.append("pageSize", pageSize);
-//   formdata.append("status", status);
-//   return requst({
-//     url: `/api/merchant/order/searchByTime`,
-//     method: "POST",
-//     data: formdata,
-//   });
-// }
-//
-// export function get_activity_list() {
-//   return requst({
-//     url: `/api/web_public/get_activity_list`,
-//     method: "GET",
-//     params: {
-//       accountId: localStorage.getItem("id"),
-//     //   accountId: 48,
-//       status: 0,
-//     },
-//   });
-// }
+
+export function search_consult(content) {
+  //搜索咨询相关内容
+  const formdata = new FormData();
+  formdata.append("id", parseInt(localStorage.getItem("id")));
+  formdata.append("content", content);
+  return requst({
+    url: `/api/web/consult/search`,
+    method: "POST",
+    data: formdata,
+  });
+}
+
+export function reply_consult(consultId, reply) {
+  //商家回复某条咨询
+  const formdata = new FormData();
+  formdata.append("id", parseInt(localStorage.getItem("id")));
+  formdata.append("consultId", consultId);
+  formdata.append("reply", reply);
+  return requst({
+    url: `/api/web/consult/reply`,
+    method: "POST",
+    data: formdata,
+  });
+}
--- src/views/business/childComponents/consult/Consult_evaluation.vue
+++ src/views/business/childComponents/consult/Consult_evaluation.vue
@@ -13,9 +13,11 @@
       >
     </div>
     <div v-show="isActive == 'allReply'" class="searchConsult">
-      <input placeholder="请输入活动名称或问题关键词" type="text" /><button>
-        搜索
-      </button>
+      <input
+        ref="search_content"
+        placeholder="请输入活动名称或问题关键词"
+        type="text"
+      /><button @click="searchClick">搜索</button>
     </div>
     <div class="consult">
       <div
@@ -39,7 +41,8 @@
             <span class="item">{{ item.reply }}</span>
           </div>
           <div class="option">
-            <span class="reply">回复</span
+            <span class="reply" @click="replyBoxShow(item.id, item.index)"
+              >回复</span
             ><span class="delete" @click="delete_consult">删除</span>
           </div>
           <div class="show">
@@ -66,6 +69,14 @@
       @before="toPage"
       @after="toPage"
     />
+    <div v-if="isOnReply" class="wrapper">
+      <div class="reply_box">
+        <div class="title">回复</div>
+        <textarea ref="textBox" class="textBox" placeholder="请输入"></textarea>
+        <button class="cancel" @click="isOnReply = 0">取消</button
+        ><button class="determine" @click="replyConsult">确定</button>
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -76,6 +87,8 @@ import {
   get_no_reply,
   show_consult,
   delete_consult,
+  search_consult,
+  reply_consult,
 } from "network/business/business.js";
 
 export default {
@@ -86,6 +99,7 @@ export default {
       isActive: "noReply",
       no_reply_consult: [], //未回复咨询
       all_consult: [], //所有咨询
+      search_consult: [], //所有搜索咨询
       pageNow: 1, //toPage的当前页
       pageTotal: 1, //toPage的总页数
       showNum: 2, //展示数量
@@ -93,26 +107,33 @@ export default {
       flag_get_all: 1, //获取所有咨询的节流阀
       pageTotal_no_reply: 1, //未回复的总页数
       pageTotal_all: 1, //所有咨询的总页数
+      pageTotal_search: 1, //所有搜索咨询的总页数
+      isOnSearch: 0, //是否处于搜索的内容
+      isOnReply: 0, //是否是处于回复状态
+      reply_consult_id: -1, //回复评论的id
+      reply_consult_index: -1, //回复评论的index
+      isChange: 0, //数据是否已经发生改变
     };
   },
   methods: {
     typeClick(type) {
       //切换
       this.isActive = type;
+      if (this.isChange) {
+        console.log("数据已经发生改变");
+        this.isChange = 0;
+        if (type == "allReply") {
+          this.flag_get_all = 0;
+          this.getAllConsult();
+        } else {
+          this.getNoConsult();
+        }
+      }
       if (type == "allReply") {
         //所有咨询
         if (this.flag_get_all) {
           //所有咨询是否已经请求过了
-          get_all().then((res) => {
-            this.all_consult = res.data.data.map((item, index) => {
-              item.index = index;
-              return item;
-            });
-            this.showNow = this.all_consult.slice(0, 2);
-            this.pageTotal_all = this.pageTotal =
-              this.all_consult.length / this.showNum;
-            this.flag_get_all = 0;
-          });
+          this.getAllConsult();
         } else {
           this.showNow = this.all_consult.slice(0, 2);
           this.pageTotal = this.pageTotal_all; //重置toPage
@@ -122,29 +143,18 @@ export default {
         this.showNow = this.no_reply_consult.slice(0, 2);
         this.pageTotal = this.pageTotal_no_reply; //重置toPage
       }
+      this.isOnSearch = 0;
+      this.$refs.search_content.value = "";
       this.pageNow = 1; //重置toPage
     },
     toPage(pageInput) {
       this.pageNow = pageInput;
-      this.isActive == "noReply"
-        ? (this.showNow = this.no_reply_consult
-            .map((item, index) => {
-              item.index = index;
-              return item;
-            })
-            .slice(
-              (pageInput - 1) * this.showNum,
-              (pageInput - 1) * this.showNum - -2
-            ))
-        : (this.showNow = this.all_consult
-            .map((item, index) => {
-              item.index = index;
-              return item;
-            })
-            .slice(
-              (pageInput - 1) * this.showNum,
-              (pageInput - 1) * this.showNum - -2
-            ));
+      this.showNow =
+        this.isActive == "noReply"
+          ? this.getShowList(this.no_reply_consult)
+          : this.isOnSearch
+          ? this.getShowList(this.search_consult)
+          : this.getShowList(this.all_consult);
     },
     showClick(e) {
       //是否展示该条咨询
@@ -155,9 +165,15 @@ export default {
           this.no_reply_consult[index].show =
             this.no_reply_consult[index].show == 1 ? 0 : 1;
         } else {
-          this.all_consult[index].show =
-            this.all_consult[index].show == 1 ? 0 : 1;
+          if (this.isOnSearch) {
+            this.search_consult[index].show =
+              this.search_consult[index].show == 1 ? 0 : 1;
+          } else {
+            this.all_consult[index].show =
+              this.all_consult[index].show == 1 ? 0 : 1;
+          }
         }
+        this.isChange = 1;
       });
     },
     delete_consult(e) {
@@ -172,25 +188,105 @@ export default {
           this.pageTotal_no_reply = this.pageTotal =
             this.no_reply_consult.length / this.showNum;
         } else {
-          this.all_consult.splice(index, 1);
-          this.toPage(this.pageNow);
-          this.pageTotal_all = this.pageTotal =
-            this.all_consult.length / this.showNum;
+          if (this.isOnSearch) {
+            this.search_consult.splice(index, 1);
+            this.toPage(this.pageNow);
+            this.pageTotal_search = this.pageTotal =
+              this.search_consult.length / this.showNum;
+          } else {
+            this.all_consult.splice(index, 1);
+            this.toPage(this.pageNow);
+            this.pageTotal_all = this.pageTotal =
+              this.all_consult.length / this.showNum;
+          }
         }
+        this.isChange = 1;
       });
+    },
+    searchClick() {
+      this.isOnSearch = 1;
+      let value = this.$refs.search_content.value;
+      if (value) {
+        console.log(value);
+        search_consult(value).then((res) => {
+          console.log(res.data.data);
+          this.search_consult = res.data.data.map((item, index) => {
+            item.index = index;
+            return item;
+          });
+          this.showNow = this.search_consult.slice(0, 2);
+          this.pageNow = 1;
+          this.pageTotal_search = this.pageTotal =
+            this.search_consult.length / this.showNum;
+        });
+      }
+      // this.$refs.search_content.value = "";
+    },
+    replyConsult() {
+      reply_consult(this.reply_consult_id, this.$refs.textBox.value).then(
+        (res) => {
+          if (this.isActive == "noReply") {
+            this.no_reply_consult[this.reply_consult_index].reply =
+              this.$refs.textBox.value;
+          } else {
+            if (this.isOnSearch) {
+              this.search_consult[this.reply_consult_index].reply =
+                this.$refs.textBox.value;
+            } else {
+              this.all_consult[this.reply_consult_index].reply =
+                this.$refs.textBox.value;
+            }
+          }
+          this.isOnReply = 0;
+          this.isChange = 1;
+        }
+      );
+    },
+    replyBoxShow(consult_id, index) {
+      this.reply_consult_id = consult_id;
+      this.reply_consult_index = index;
+      this.isOnReply = 1;
+    },
+    getAllConsult() {
+      get_all().then((res) => {
+        //请求所有咨询
+        this.all_consult = res.data.data.map((item, index) => {
+          item.index = index;
+          return item;
+        });
+        this.showNow = this.all_consult.slice(0, 2);
+        this.pageTotal_all = this.pageTotal =
+          this.all_consult.length / this.showNum;
+        this.flag_get_all = 0;
+      });
+    },
+    getNoConsult() {
+      get_no_reply().then((res) => {
+        //请求未回复的咨询
+        this.no_reply_consult = res.data.data.map((item, index) => {
+          item.index = index;
+          return item;
+        });
+        this.showNow = this.no_reply_consult.slice(0, 2);
+        this.pageTotal_no_reply = this.pageTotal =
+          this.no_reply_consult.length / this.showNum;
+      });
+    },
+    getShowList(arr) {
+      return arr
+        .map((item, index) => {
+          item.index = index;
+          return item;
+        })
+        .slice(
+          (this.pageNow - 1) * this.showNum,
+          (this.pageNow - 1) * this.showNum - -2
+        );
     },
   },
   mounted() {
-    get_no_reply().then((res) => {
-      //请求未回复的咨询
-      this.no_reply_consult = res.data.data.map((item, index) => {
-        item.index = index;
-        return item;
-      });
-      this.showNow = this.no_reply_consult.slice(0, 2);
-      this.pageTotal_no_reply = this.pageTotal =
-        this.no_reply_consult.length / this.showNum;
-    });
+    this.isOnSearch = 0;
+    this.getNoConsult();
   },
 };
 </script>
@@ -365,5 +461,75 @@ export default {
   vertical-align: middle;
   cursor: pointer;
 }
-/* */
+.wrapper {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  z-index: 99999;
+}
+.reply_box {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  padding-left: 32px;
+  width: 487px;
+  height: 334px;
+  background: #ffffff;
+  border: 2px solid #d7d7d7;
+  box-shadow: 0px 3px 15px 3px rgba(0, 0, 0, 0.03);
+  border-radius: 4px;
+}
+.reply_box .title {
+  margin: 22px 0 32px 0;
+  height: 18px;
+  font-size: 18px;
+  font-weight: bold;
+  color: #333333;
+}
+.reply_box .title::after {
+  position: relative;
+  top: 20px;
+  left: -10px;
+  content: "";
+  display: block;
+  width: 443px;
+  height: 2px;
+  background: #dddddd;
+}
+.reply_box .textBox {
+  width: 423px;
+  height: 166px;
+  padding: 8px 10px;
+  border: 1px solid #dddddd;
+  resize: none;
+  outline: none;
+}
+.reply_box button {
+  margin-top: 32px;
+  width: 62px;
+  height: 29px;
+  border: none;
+  border-radius: 4px;
+  font-size: 15px;
+  font-family: PingFang SC;
+  font-weight: 400;
+  color: #ffffff;
+  outline: none;
+  cursor: pointer;
+}
+.reply_box .cancel {
+  margin-left: 117px;
+  margin-right: 66px;
+  background: #999999;
+}
+.reply_box .determine {
+  background: #ff7000;
+}
+/*
+
+      
+ */
 </style>
